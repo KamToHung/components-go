77d04be16bc2c2371c93a0b2f870a0cd6f2d0af2
KamToHung <kamtohung@gmail.com>
2023-11-03 11:15:09 +0800

    feat: add option test

---
+++ dispatcher/option_test.go
@@ -0,0 +1,126 @@
+package dispatcher
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestOptConsumerBufferSize(t *testing.T) {
+	type args struct {
+		s int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptConsumerBufferSize(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptConsumerBufferSize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOptConsumerConcurrency(t *testing.T) {
+	type args struct {
+		c int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptConsumerConcurrency(tt.args.c); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptConsumerConcurrency() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOptConsumerConfig(t *testing.T) {
+	type args struct {
+		option ConsumerOption
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptConsumerConfig(tt.args.option); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptConsumerConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOptProducerBufferSize(t *testing.T) {
+	type args struct {
+		s int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptProducerBufferSize(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptProducerBufferSize() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOptProducerConcurrency(t *testing.T) {
+	type args struct {
+		c int
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptProducerConcurrency(tt.args.c); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptProducerConcurrency() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestOptProducerConfig(t *testing.T) {
+	type args struct {
+		option ProducerOption
+	}
+	tests := []struct {
+		name string
+		args args
+		want Option
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := OptProducerConfig(tt.args.option); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OptProducerConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
