6b6402bfa4265b225203571438294d5888c8efcd
KamToHung <kamtohung@gmail.com>
2023-11-09 14:02:39 +0800

    feat: add Consume

--- dispatcher/dispatcher_test.go
+++ dispatcher/dispatcher_test.go
@@ -3,7 +3,6 @@ package dispatcher
 import (
 	"context"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -54,76 +53,15 @@ func (t TestRunner) Consume(message Message) {
 }
 
 func TestDispatcher_GetConsumeMessageCount(t *testing.T) {
-	type fields struct {
-		producerConfig ProducerConfig
-		consumerConfig ConsumerConfig
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   uint64
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				producerConfig: tt.fields.producerConfig,
-				consumerConfig: tt.fields.consumerConfig,
-			}
-			assert.Equalf(t, tt.want, d.GetConsumeMessageCount(), "GetConsumeMessageCount()")
-		})
-	}
+	// TODO: Add test cases.
 }
 
 func TestDispatcher_GetSendMessageCount(t *testing.T) {
-	type fields struct {
-		producerConfig ProducerConfig
-		consumerConfig ConsumerConfig
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   uint64
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				producerConfig: tt.fields.producerConfig,
-				consumerConfig: tt.fields.consumerConfig,
-			}
-			assert.Equalf(t, tt.want, d.GetSendMessageCount(), "GetSendMessageCount()")
-		})
-	}
+	// TODO: Add test cases.
 }
 
 func TestDispatcher_Start(t *testing.T) {
-	type fields struct {
-		producerConfig ProducerConfig
-		consumerConfig ConsumerConfig
-	}
-	type args struct {
-		ctx     context.Context
-		configs []interface{}
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := &Dispatcher{
-				producerConfig: tt.fields.producerConfig,
-				consumerConfig: tt.fields.consumerConfig,
-			}
-			d.Start(tt.args.ctx, tt.args.configs...)
-		})
-	}
+	// TODO: Add test cases.
 }
 
 func TestNew(t *testing.T) {
@@ -131,58 +69,13 @@ func TestNew(t *testing.T) {
 }
 
 func Test_calculateIndex(t *testing.T) {
-	type args struct {
-		key  string
-		size int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, calculateIndex(tt.args.key, tt.args.size), "calculateIndex(%v, %v)", tt.args.key, tt.args.size)
-		})
-	}
+	// TODO: Add test cases.
 }
 
 func Test_consumerProcess(t *testing.T) {
-	type args struct {
-		consumerConfig *ConsumerConfig
-		channels       []chan Message
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			consumerProcess(tt.args.consumerConfig, tt.args.channels)
-		})
-	}
+	// TODO: Add test cases.
 }
 
 func Test_producerProcess(t *testing.T) {
-	type args struct {
-		producerConfig   *ProducerConfig
-		ctx              context.Context
-		consumerChannels []chan Message
-		configs          []interface{}
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			producerProcess(tt.args.producerConfig, tt.args.ctx, tt.args.consumerChannels, tt.args.configs...)
-		})
-	}
+	// TODO: Add test cases.
 }
