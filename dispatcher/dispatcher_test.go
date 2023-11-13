d3d89f23ba66599079bf2793c82c8114a2914282
KamToHung <kamtohung@gmail.com>
2023-11-13 16:10:43 +0800

    test: add TestDispatcher_Start

--- dispatcher/dispatcher_test.go
+++ dispatcher/dispatcher_test.go
@@ -1,6 +1,11 @@
 package dispatcher
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
 )
 
@@ -16,11 +21,38 @@ func TestDispatcher_GetSendMessageCount(t *testing.T) {
 }
 
 func TestDispatcher_Start(t *testing.T) {
-	// TODO: Add test cases.
+	parentCtx := context.Background()
+	ctx, cancel := context.WithCancel(parentCtx)
+	// 创建一个通道来接收操作系统信号
+	sigs := make(chan os.Signal, 1)
+	// 注册感兴趣的信号
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// 启动一个goroutine来等待信号
+	go func() {
+		sig := <-sigs
+		fmt.Println("接收到信号:", sig)
+		cancel()
+		os.Exit(0)
+	}()
+	d := New(
+		OptProducerConfig(func() Producer {
+			return &TestRunner{}
+		}),
+		OptProducerConcurrency(20),
+		OptProducerBufferSize(1024),
+		OptConsumerConfig(func() Consumer {
+			return &TestRunner{}
+		}),
+		OptProducerConcurrency(10),
+		OptProducerBufferSize(1024),
+	)
+	d.Start(ctx, &TestConfig{name: "test"})
 }
 
 func TestNew(t *testing.T) {
 	// TODO
+
 }
 
 func Test_calculateIndex(t *testing.T) {
