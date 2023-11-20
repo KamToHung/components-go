9ed94f7a02c575e273dbbf533fe0bac160ac8494
KamToHung <kamtohung@gmail.com>
2023-11-20 23:41:26 +0800

    test: add TestDispatcher_Start

--- dispatcher/dispatcher_test.go
+++ dispatcher/dispatcher_test.go
@@ -3,6 +3,7 @@ package dispatcher
 import (
 	"context"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,6 +15,34 @@ type TestConfig struct {
 }
 
 func TestDispatcher_GetConsumeMessageCount(t *testing.T) {
+	parentCtx := context.Background()
+	ctx, cancel := context.WithCancel(parentCtx)
+	// 创建一个通道来接收操作系统信号
+	sigs := make(chan os.Signal, 1)
+	// 注册感兴趣的信号
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// 启动一个goroutine来等待信号
+	go func() {
+		sig := <-sigs
+		fmt.Println("接收到信号:", sig)
+		cancel()
+		os.Exit(0)
+	}()
+	d := New(
+		OptProducerConfig(func() Producer {
+			return &TestRunner{}
+		}),
+		OptProducerConcurrency(20),
+		OptProducerBufferSize(1024),
+		OptConsumerConfig(func() Consumer {
+			return &TestRunner{}
+		}),
+		OptProducerConcurrency(10),
+		OptProducerBufferSize(1024),
+	)
+	d.Start(ctx, &TestConfig{name: "test"})
+	assert.Equal(t, d.GetConsumeMessageCount(), 0)
 }
 
 func TestDispatcher_GetSendMessageCount(t *testing.T) {
